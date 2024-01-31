c13f6b98167d6c9039c22289e071a5c60a84d10a
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2024-01-31 15:55:06 +0100

    added module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from '../app.component';
-import { WeatherComponent } from '../weather/weather.component';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [AppComponent, WeatherComponent],
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,7 +3,6 @@ import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
-
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css',
 })
